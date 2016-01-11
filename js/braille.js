f9f256080a25864fa22e26545088350bdbc1cbf5
ArnaudFabre <arnaud.fabre@gmail.com>
2016-01-12 00:31:11 +0100

    Add a BRF converter and refactorize portions to BrailleCell

--- js/braille.js
+++ js/braille.js
@@ -1,3 +1,242 @@
+var BrailleCell = (function (map) {
+
+    var NONE = 0;
+    var EXP = -1; // Exposant
+    var SIGN = -2; // Sign prefix
+    var MAJ = -3; // Upper Case
+    var CUR = -4; // Currency prefix
+    var NUM = -5; // number prefix
+    var ING = -6; // ing
+    var LET = -7; // letter prefix
+    var IT = -8; // Italique
+
+    var prefix = NONE;
+    var first = false;
+    var mapping;
+
+    var braille_fr = [
+    ' ', 'a', ',', 'b', "'", 'k', ';', ['l', NONE, NONE, '£'], EXP, 'c',
+    'i', 'f', ['/', '/'], 'm', ['s', NONE, NONE, '$'], 'p', SIGN, ['e', NONE, NONE, '€'], [':', ':'], 'h',
+    ['*', '*'], 'o', ['!', '+'], 'r', CUR, 'd', 'j', 'g', '@', 'n',
+    't', 'q', NUM, ['â', '1'], '?', ['ê', '2'], '-', 'u', '(', 'v',
+    MAJ, ['î', '3'], ['œ', '9'], ['ë', '6'], [ING, NONE, '%'], ['x', 'x'], 'è', 'ç', LET, ['û', '5'],
+    '.', ['ü', '8'], ')', 'z', ['"', '='], 'à', IT, ['ô', '4'], 'w', ['ï', '7'],
+    '0', 'y', 'ù', ['é', NONE, '&']
+    ];
+
+    var brf = [
+    ' ', 'A', '1', 'B', "'", 'K', '2', 'L', '@', 'C', // 10
+    'I', 'F', '/', 'M', 'S', 'P', '"', 'E', '3', 'H', // 20
+    '9', 'O', '6', 'R', '^', 'D', 'J', 'G', '>', 'N', // 30
+    'T', 'Q', ',', '*', '5', '<', '-', 'U', '8', 'V', // 40
+    '.', '%', '[', '$', '+', 'X', '!', '&', ';', ':', // 50
+    '4', '\\', '0', 'Z', '7', '(', '_', '?', 'W', ']', // 60
+    '#', 'Y', ')', '='
+    ];
+
+    var cell = [0, 0, 0, 0, 0, 0]; // Braille Cell
+
+    if (mapping) {
+        mapping = map;
+    } else {
+        mapping = braille_fr;
+    }
+
+    function getBraille() {
+        var b = 0;
+        for (var i = 0; i < 6; i++) {
+            b += Math.pow(2, i) * cell[i];
+        }
+        return b;
+    }
+
+    function getDots() {
+        var p = '';
+        switch (prefix) {
+        case NUM:
+            if (first) {
+                p = '6';
+            }
+            break;
+        case MAJ:
+            p = '4-6';
+            break;
+        case CUR:
+            p = '4-5';
+            break;
+        case SIGN:
+            p = '5';
+            break;
+        }
+        var d = '';
+        for (var i = 0; i < 6; i++) {
+            if (cell[i]) {
+                if (d !== '')
+                    d += '-';
+                d += (i + 1);
+            }
+        }
+        if (prefix)
+            d = p + ' ' + d;
+        if (d === '') {
+            return '0';
+        }
+        return d;
+    }
+
+    function getBlack() {
+        var n = getBraille();
+        var l = mapping[n];
+        console.log(prefix);
+        console.log(l);
+        if (Array.isArray(l)) {
+            switch (prefix) {
+            case CUR:
+                return l[3];
+            case SIGN:
+                return l[2];
+            case NUM:
+                return l[1];
+            default:
+                return l[0];
+            }
+        }
+
+        switch (prefix) {
+        case MAJ:
+            return l.toUpperCase();
+        }
+        return l;
+    }
+
+    function getBrf() {
+        var n = getBraille();
+        switch (prefix) {
+        case CUR:
+            return '^' + brf[n];
+        case SIGN:
+            return '"' + brf[n];
+        case MAJ:
+            return '.' + brf[n];
+        case NUM:
+            if (!first)
+                return brf[n];
+            else
+                return ',' + brf[n];
+        }
+        return brf[n];
+    }
+
+    function get(type) {
+        type = type || 'black';
+        switch (type) {
+        case 'black':
+            return getBlack();
+        case 'brf':
+            return getBrf();
+        case 'braille':
+            return getBraille();
+        case 'dots':
+            return getDots();
+        }
+    }
+
+    function isNumException(n) {
+        return (['x', '*', ':', '/'].indexOf(n) != -1);
+    }
+
+    function inCol(char, col) {
+        for (var i = 0; i < mapping.length; i++) {
+            if (Array.isArray(mapping[i])) {
+                if (mapping[i][col] === char) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    function search(char) {
+        for (var i = 0; i < mapping.length; i++) {
+            if (Array.isArray(mapping[i])) {
+                for (var j = 0; j < mapping[i].length; j++) {
+                    if (mapping[i][j] === char) {
+                        console.log(mapping[i][j]);
+                        return i;
+                    }
+                }
+            } else {
+                if (mapping[i] === char) {
+                    return i;
+                }
+            }
+        }
+        return undefined;
+    }
+
+    function set(dot, value) {
+        if (typeof dot === 'number') {
+            cell[dot] = value;
+        } else
+        if (typeof dot === 'string') {
+            var rest, letter = dot[0];
+            first = false;
+            if (dot[0].toUpperCase() === dot[0] && dot[0] !== dot[0].toLowerCase()) {
+                // Detect Upper Case
+                prefix = MAJ;
+                letter = dot[0].toLowerCase();
+            } else {
+                // Detect number
+                var nan = false;
+                if (inCol(dot[0], 1)) {
+                    if (prefix === NUM) {
+                        first = false;
+                    } else {
+                        first = true;
+                        if (isNumException(dot[0])) {
+                            nan = true;
+                            first = false;
+                        }
+                    }
+                    if (nan) {
+                        prefix = NONE;
+                    } else {
+                        prefix = NUM;
+                    }
+                } else if (inCol(dot[0], 2)) {
+                    prefix = SIGN;
+                } else if (inCol(dot[0], 3)) {
+                    prefix = CUR;
+                } else {
+                    prefix = NONE;
+                }
+            }
+            rest = search(letter);
+            for (var i = 5; i >= 0; i--) {
+                cell[i] = Math.floor(rest / Math.pow(2, i));
+                rest = rest - cell[i] * Math.pow(2, i);
+            }
+        } else if (Array.isArray(dot)) {
+            for (var i = 0; i < 6; i++) {
+                cell[i] = dot[i];
+            }
+        }
+        return this;
+    }
+
+    function reset() {
+        for (var i = 0; i < 6; i++) {
+            this.cell[i] = 0;
+        }
+    }
+
+    return {
+        get: get,
+        set: set,
+        reset: reset,
+    };
+});
+
 var cell = [0, 0, 0, 0, 0, 0]; // Braille Cell
 
 var pushed = 0; // Touch are all released
@@ -157,34 +396,26 @@ var questions_fr = {
 // -3 : Uppercase
 // -4 : end of version
 
-var brf = [
-    ' ', 'a', '1', 'b', "'", 'k', '2', 'l', '@', 'c', // 10
-    'i', 'f', '/', 'm', 's', 'p', '"', 'e', '3', 'h', // 20
-    '9', 'o', '6', 'r', '^', 'd', 'j', 'g', '>', 'n', // 30
-    't', 'q', ',', '*', '5', '<', '-', 'u', '8', 'v', // 40
-    '.', '%', '[', '$', '+', 'x', '!', 'ç', ';', ':', // 50
-    '4', '\\', '0', 'z', '7', '(', '_', '?', 'w', ']', // 60
-    '#', 'y', ')', '='
-];
+var MAJ = -3;
 
 var braille_fr = [
     ' ', 'a', ',', 'b', "'", 'k', ';', 'l', -1, 'c',
-    'i', 'f', ' ', 'm', 's', 'p', -1, 'e', ':', 'h',
+    'i', 'f', '/', 'm', 's', 'p', -1, 'e', ':', 'h',
     '*', 'o', '!', 'r', -1, 'd', 'j', 'g', -4, 'n',
     't', 'q', -2, 'â', '?', 'ê', '-', 'u', '(', 'v',
-    -3, 'î', 'œ', 'ë', -1, 'x', 'è', 'ç', -1, 'û',
+    MAJ, 'î', 'œ', 'ë', -1, 'x', 'è', 'ç', -1, 'û',
     '.', 'ü', ')', 'z', '"', 'à', -1, 'ô', 'w', 'ï',
     -5, 'y', 'ù', 'é'
 ];
 
 var braille_num = [
     ' ', 'a', ',', 'b', "'", 'k', ';', 'l', -1, 'c',
-    'i', 'f', ' ', 'm', 's', 'p', -1, 'e', ':', 'h',
+    'i', 'f', '/', 'm', 's', 'p', -1, 'e', ':', 'h',
     '*', 'o', '!', 'r', -1, 'd', 'j', 'g', -4, 'n',
     't', 'q', '', '1', '?', '2', '-', 'u', '(', 'v',
-    '', '3', '9', '6', -1, 'x', 'è', 'ç', -1, '5',
+    MAJ, '3', '9', '6', -1, 'x', 'è', 'ç', -1, '5',
     '.', '8', ')', 'z', '"', 'à', -1, '4', 'w', '7',
-    -5, 'y', 'ù', 'é'
+    '0', 'y', 'ù', 'é'
 ];
 var current_braille = braille_num;
 
@@ -192,21 +423,46 @@ var current_braille = braille_num;
 $('#trans').click(function () {
     var input = $('#input').val();
     $('#output').empty();
-    $('#output').empty();
+    $('#output2').empty();
     var i = 0;
     var count = 0;
+
+    var bc = new BrailleCell();
+
+    var first = true;
     for (i = 0; i < input.length; count++, i++) {
-        console.log(count);
+        if ((count % 32 === 0 && !first) || input[i] === '\n') {
+            $('#output').append('\n');
+            $('#output2').append('</br>');
 
-        if (count % 32 === 0 || input[i] === '\n') {
-            $('#output').append('<br/>');
             count = 0;
+        } else {
+            bc.set(input[i]);
+            $('#output').append(bc.get('brf'));
+            $('#output2').append(bc.get('dots'));
+            $('#output2').append(' ');
         }
-
-        var pos = brf.indexOf(input[i].toLowerCase());
-        $('#output').append(current_braille[pos]);
+        first = false;
     }
 });
+
+function downloadBRF(args) {
+    var data, filename, link;
+    var brf = $('#output').html();
+
+    if (brf == null) return;
+
+    filename = args.filename || 'export.brf';
+
+    var contentType = 'text/brf';
+    var blob = new Blob([brf], {
+        'type': contentType
+    });
+    var a = document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = filename;
+    a.click();
+}
 
 function setAlphabet() {
     var dico = current_braille.map(function (l) {
